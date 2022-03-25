b691f277eecc7c4f5ec93ccfc49e854e5baa0c2a
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-03-25 12:42:26 -0400

    indexedDB setup

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-
+import { idbPromise } from "../../utils/helpers";
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
@@ -20,8 +20,12 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach(product => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
   
   function filterProducts() {
     if (!currentCategory) {
