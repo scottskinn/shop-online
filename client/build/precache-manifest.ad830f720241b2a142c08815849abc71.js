6aa79b51d830c298e4e2e15190e257cdd8ec6b5c
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-03-29 16:57:03 -0400

    heroku build

---
+++ client/build/precache-manifest.ad830f720241b2a142c08815849abc71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d114a3e417e5b4b3e435a7ead68018cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e6d8ca7198d937d57a",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "04bfe56b3acc865981ea",
+    "url": "/static/js/2.9eaf7eb6.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9eaf7eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e6d8ca7198d937d57a",
+    "url": "/static/js/main.f56e2b0a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
